73def65bba4353f8b2ac8866e6082931ce4737d5
Lukas da Rosa <lukas-sr@hotmail.com>
2024-06-13 14:49:43 -0300

    Simplify GrabConsole.cs and modify access to the buffer

--- GrabConsole.cs
+++ GrabConsole.cs
@@ -14,151 +14,88 @@ namespace DALSA.SaperaLT.Examples.NET.CSharp.GrabConsole
 {
     class GrabConsole
    {
-        static SapAcquisition Acq = null;
-        static SapAcqDevice AcqDevice = null;
-        static SapBuffer Buffers = null;
-        static SapTransfer Xfer = null;
-        static SapView View = null;
-        static int countFrame = 0;
-        static List<Int16[,]> frames = new List<Int16[,]>();
+        public static SapAcquisition Acq = null;
+        public static SapAcqDevice AcqDevice = null;
+        public static SapBuffer Buffers = null;
+        public static SapTransfer Xfer = null;
+        public static SapView View = null;
+        public static int numFrames = 1;
+        public static int sizeArr = 0;
+        public static Int16[,] framesArr = null;
+        public MyAcquisitionParams acqParams;
+        private static int countFrame = 0;
 
         [System.Runtime.InteropServices.DllImport("kernel32.dll")]
         private static extern bool AllocConsole();
 
-        static void Xfer_XferNotify(object sender, SapXferNotifyEventArgs args)
-        {
-            // refresh view
-            SapView View = args.Context as SapView;
-            View.Show();
-
-            // save Buffer
-            Console.WriteLine("Buffers.Count: " + Buffers.Count.ToString() + " " +
-                              "Buffers.Index: " + Buffers.Index.ToString()); 
-            int width = Buffers.Width;
-            int height = 1;//Buffers.Height;
-            Stopwatch stopwatch = new Stopwatch();
-
-            stopwatch.Start();
-            Buffers.GetAddress(out IntPtr buffAddress);
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed.TotalSeconds.ToString() + " s");
-
-            //SaveImageData2File(width, height, buffAddress);
-            SaveFrames2LabView(width, height, buffAddress);
-
-        }
-
-        private static void SaveImageData2File(int width, int height, IntPtr buffAddress)
-        {
-            Int16[] imageData = new Int16[width * height];
-            Marshal.Copy(buffAddress, imageData, 0, width * height);
-
-            string filePath = @"C:\temp\matrix" + countFrame + ".txt";
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                for (int i = 0; i < height; i++)
-                {
-                    for (int j = 0; j < width; j++)
-                    {
-                        writer.Write(imageData[i+j].ToString() + " ");
-                    }
-                    writer.WriteLine();
-                }
-            }
-        }
-
-        private static void SaveFrames2LabView(int width, int height, IntPtr buffAddress)
-        {
-            Int16[] imageData = new Int16[height * width];
-            Int16[,] matrix = new Int16[height, width];
-            Marshal.Copy(buffAddress, imageData, 0, height * width);
-
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    matrix[i, j] = imageData[i + j];
-                }
-            }
-
-            frames.Insert(countFrame, matrix);
-            countFrame++;
-        }
-        
-        private static void Frame2File()
-        {
-            string filePath = @"C:\temp\frames.txt";
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                foreach(var mat in frames)
-                {
-                    writer.WriteLine("Init > ");
-                    for (int i = 0; i < mat.GetLength(0); i++)
-                    {
-                        for ( int j = 0; j < mat.GetLength(1); j++)
-                        {
-                            writer.Write(mat[i, j].ToString() + " ");
-                        }
-                        writer.WriteLine();
-                    }
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            // This is only meaningful for .NET 5 and up in order to show the console, since the project
-            // needs to be configured as a Windows application with Windows Forms when it uses SapView
-            AllocConsole();
-            Console.WriteLine("Sapera Console Grab Example (C# version)\n");
+            string path = @"C:\temp\IC-47_TDI.ccf";
 
-            MyAcquisitionParams acqParams = new MyAcquisitionParams();
-            // Call GetOptions to determine which acquisition device to use and which config
-            // file (CCF) should be loaded to configure it.
-            if (!GetOptions(args, acqParams))
+            GrabConsole gbConsole = new GrabConsole();
+            gbConsole.setConfigFile(path);
+            Console.Write("Number of Frames: ");
+            numFrames = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            gbConsole.StartGrabFramesTDI(numFrames);
+
+            Console.WriteLine(framesArr[0, 0].ToString());
+        }
+        public GrabConsole()
+        {
+            acqParams = new MyAcquisitionParams
             {
-               Console.WriteLine("\nPress any key to terminate\n");
-               Console.ReadKey(true);
-               return;
+                ResourceIndex = 0,
+                ServerName = "Xtium-CLHS_PX8_1"
+            };
+        }
+        public bool setConfigFile(string filePath)
+        {
+            acqParams.ConfigFileName = filePath;
+
+            if ((acqParams.ConfigFileName != null) && (acqParams.ServerName.Equals("Xtium-CLHS_PX8_1")))
+            {
+                return true;
             }
 
+            return false;
+        }
+
+        public void StartGrabFramesTDI(int numFrames)
+        {
             SapLocation loc = new SapLocation(acqParams.ServerName, acqParams.ResourceIndex);
 
             if (SapManager.GetResourceCount(acqParams.ServerName, SapManager.ResourceType.Acq) > 0)
             {
-               Acq = new SapAcquisition(loc, acqParams.ConfigFileName);
-               Buffers = new SapBufferWithTrash(2, Acq, SapBuffer.MemoryType.ScatterGather);
-               Xfer = new SapAcqToBuf(Acq, Buffers);
+                Acq = new SapAcquisition(loc, acqParams.ConfigFileName);
+                Buffers = new SapBufferWithTrash(2, Acq, SapBuffer.MemoryType.ScatterGather);
+                Xfer = new SapAcqToBuf(Acq, Buffers);
 
-               // Create acquisition object
-               if (!Acq.Create())
-               {
-                  Console.WriteLine("Error during SapAcquisition creation!\n");
-                  DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
-                  return;
-               }
-               Acq.EnableEvent(SapAcquisition.AcqEventType.StartOfFrame);
-
+                // Create acquisition object
+                if (!Acq.Create())
+                {
+                    DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
+                    return;
+                }
+                Acq.EnableEvent(SapAcquisition.AcqEventType.StartOfFrame);
             }
 
             else if (SapManager.GetResourceCount(acqParams.ServerName, SapManager.ResourceType.AcqDevice) > 0)
             {
-               AcqDevice = new SapAcqDevice(loc, acqParams.ConfigFileName);
-               Buffers = new SapBufferWithTrash(2, AcqDevice, SapBuffer.MemoryType.ScatterGather);
-               Xfer = new SapAcqDeviceToBuf(AcqDevice, Buffers);
+                AcqDevice = new SapAcqDevice(loc, acqParams.ConfigFileName);
+                Buffers = new SapBufferWithTrash(2, AcqDevice, SapBuffer.MemoryType.ScatterGather);
+                Xfer = new SapAcqDeviceToBuf(AcqDevice, Buffers);
 
-               // Create acquisition object
-               if (!AcqDevice.Create())
-               {
-                  Console.WriteLine("Error during SapAcqDevice creation!\n");
-                  DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
-                  return;
-               }
+                // Create acquisition object
+                if (!AcqDevice.Create())
+                {
+                    DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
+                    return;
+                }
             }
-            Console.WriteLine("Select how many frames: ");
-            int userInput = int.Parse(Console.ReadLine());
 
             View = new SapView(Buffers);
+
             // End of frame event
             Xfer.Pairs[0].EventType = SapXferPair.XferEventType.EndOfFrame;
             Xfer.XferNotify += new SapXferNotifyHandler(Xfer_XferNotify);
@@ -167,15 +104,17 @@ namespace DALSA.SaperaLT.Examples.NET.CSharp.GrabConsole
             // Create buffer object
             if (!Buffers.Create())
             {
-                Console.WriteLine("Error during SapBuffer creation!\n");
                 DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
                 return;
-            }    
+            }
+
+            // For TDI Case the heigth of the buffer must be equal 1
+            sizeArr = Buffers.Width * 1;
+            InitializeFrameArray(numFrames, sizeArr);
 
             // Create buffer object
             if (!Xfer.Create())
             {
-                Console.WriteLine("Error during SapTransfer creation!\n");
                 DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
                 return;
             }
@@ -183,39 +122,54 @@ namespace DALSA.SaperaLT.Examples.NET.CSharp.GrabConsole
             // Create buffer object
             if (!View.Create())
             {
-                Console.WriteLine("Error during SapView creation!\n");
                 DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
                 return;
             }
 
             //Xfer.Grab();
-            if (userInput > 0 && userInput < 100)
+            if (numFrames > 0 && numFrames < 100)
             {
-                Xfer.Snap(userInput);
+                Xfer.Snap(numFrames);
             }
             else
             {
                 Xfer.Snap();
             }
-
-            Console.WriteLine("\n\nGrab started, press a key to freeze");
-            //Console.ReadKey(true);
-            Xfer.Wait(2000);
+            Xfer.Wait(10000);
             DestroysObjects(Acq, AcqDevice, Buffers, Xfer, View);
             loc.Dispose();
         }
 
-        static bool GetOptions(string[] args, MyAcquisitionParams acqParams)
+        public virtual void Xfer_XferNotify(object sender, SapXferNotifyEventArgs args)
         {
-            // Check if arguments were passed
-            if (args.Length > 1)
-            return ExampleUtils.GetOptionsFromCommandLine(args, acqParams);
-            else
-            return ExampleUtils.GetOptionsFromQuestions(acqParams);
-        }
+            // save Buffer
+            Buffers.GetAddress(out IntPtr buffAddress);
+            SaveFrameArray(sizeArr, buffAddress);
 
+            // refresh view
+            SapView View = args.Context as SapView;
+            View.Show();
+        }
+        public static void InitializeFrameArray(int dim1, int dim2)
+        {
+            Console.WriteLine("Initializing frame array with dimension " + dim1.ToString() + "x" + dim2.ToString() + "...");
+            framesArr = (Int16[,])Array.CreateInstance(typeof(Int16), dim1, dim2);
+        }
+        public static void SaveFrameArray(int size, IntPtr buffAddress)
+        {
+            Int16[] imageData = new Int16[size];
+            Marshal.Copy(buffAddress, imageData, 0, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                framesArr[countFrame, i] = imageData[i];
+            }
+            Console.WriteLine("Saving frame nº " + (countFrame+1).ToString() + "...");
+            countFrame++;
+        }
         static void DestroysObjects(SapAcquisition acq, SapAcqDevice camera, SapBuffer buf, SapTransfer xfer, SapView view)
         {
+            Console.WriteLine("Destroying objects...");
             if (xfer != null)
             {
                 xfer.Destroy();
@@ -245,9 +199,6 @@ namespace DALSA.SaperaLT.Examples.NET.CSharp.GrabConsole
                 view.Destroy();
                 view.Dispose();
              }
-             Frame2File();
-             Console.WriteLine("\nPress any key to terminate\n");
-             Console.ReadKey(true);
         } 
    }
 }
